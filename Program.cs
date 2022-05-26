72f5caaf5f005a2b055ba3214b0610e56c9c9d04
Erick Goldberg <erick_goldberg@hotmail.com>
2022-05-25 22:57:45 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace DesafiosCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t Desafios C#");
+            for (int c = 0; c < 6; c++)
+            {
+                for (int i = 0; i <= c; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+            for (int f = 0; f <= 4; f++)
+            {
+                for (int j = 4; j > f; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
